dc23f7db3fa142ae3430e454645579f8fee6d278
Matthias Margush <matthias.margush@me.com>
2020-08-22 11:10:06 -0700

    Visual glitches

--- themes/ci.sh
+++ themes/ci.sh
@@ -1,8 +1,6 @@
 theme="CI"
 description="A CI theme for Jetbrains IDEs."
 dark="true"
-dark_uuid="0798AC7F-3DCD-4C13-9648-8F7D41188F8C"
-light_uuid="84FAEF97-9884-45D7-B405-6807505D130D"
 declare -A colors=(
   ["highlight"]="#30363D"
 
@@ -10,11 +8,11 @@ declare -A colors=(
   ["dimForeground"]="#F6F8FA"
 
   ["background"]="#24292E"
-  ["dimBackground"]="#24292E"
+  ["dimBackground"]="#2D3237"
 
   ["red"]="#F97583"
   ["green"]="#85E89D"
-  ["blue"]="#79B8FF"
+  ["blue"]="#73E3FF"
   ["purple"]="#B392F0"
   ["yellow"]="#FFEA7F"
   ["grey"]="#959DA5"
--- themes/paper.sh
+++ themes/paper.sh
@@ -6,11 +6,11 @@ light_uuid="762A8223-BB63-43DD-9C1D-11F64F7DE852"
 declare -A colors=(
   ["highlight"]="#DDD9CC"
 
-  ["foreground"]="#E8E6E2"
-  ["dimForeground"]="#F8F6F2"
+  ["foreground"]="#2A2A2A"
+  ["dimForeground"]="#3A3A3A"
 
-  ["background"]="#2A2A2A"
-  ["dimBackground"]="#3A3A3A"
+  ["background"]="#E8E6E2"
+  ["dimBackground"]="#F8F6F2"
 
   ["red"]="#775555"
   ["green"]="#557755"
@@ -18,5 +18,5 @@ declare -A colors=(
   ["purple"]="#775577"
   ["yellow"]="#777755"
 
-  ["grey"]="#999999"
+  ["grey"]="#777777"
 )
